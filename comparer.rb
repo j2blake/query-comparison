187c5ce526ddd0a2e7eb6aedfc4f3c53aab16c5d
Jim Blake <jeb228@cornell.edu>
2015-06-03 11:34:49 -0400

    Some restructuring

--- comparer.rb
+++ comparer.rb
@@ -22,7 +22,7 @@ The report
 filename1   900  1.234   124  1.500   102 11.500
 filename2  1024  6.103   124 12.105   600  4.123
 
-outputs to filename1-filename2-unique and filename2-filename1-unique
+outputs to report-filename1-filename2, diff-filename1-filename2 and diff-filename2-filename1
 
 --------------------------------------------------------------------------------
 =end
@@ -69,6 +69,11 @@ class Comparer
     raise UserInputError.new("File not found: '#{args[1]}'") unless File.exist?(args[1])
     @file1 = File.expand_path(args[0])
     @file2 = File.expand_path(args[1])
+    @base1 = File.basename(@file1)
+    @base2 = File.basename(@file2)
+    @report = File.expand_path("report-#{@base1}-#{@base2}")
+    @diff1 = File.expand_path("diff-#{@base1}-#{@base2}")
+    @diff2 = File.expand_path("diff-#{@base2}-#{@base1}")
   end
 
   def parse_files()
@@ -97,16 +102,12 @@ class Comparer
   end
 
   def dump_remainders()
-    base1 = File.basename(@file1)
-    base2 = File.basename(@file2)
-    r1 = File.expand_path("diff-#{base1}-#{base2}", File.dirname(@file1))
-    r2 = File.expand_path("diff-#{base2}-#{base1}", File.dirname(@file2))
-    File.open(r1, 'w') do |f|
+    File.open(@diff1, 'w') do |f|
       @unique1.each do |q|
         f.puts("#{q[1]} #{q[0]}")
       end
     end
-    File.open(r2, 'w') do |f|
+    File.open(@diff2, 'w') do |f|
       @unique2.each do |q|
         f.puts("#{q[1]} #{q[0]}")
       end
@@ -118,12 +119,15 @@ class Comparer
   end
 
   def report()
-    base1 = File.basename(@file1)
-    base2 = File.basename(@file2)
-    w = [base1.size, base2.size].max
+    w = [@base1.size, @base2.size].max
+    File.open(@report, 'w') do |f|
+      f.puts sprintf("%#{w}s     total        common       unique", ' ')
+      f.puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", @base1, @list1.size, sum_times(@list1), @common_count, @common_time_1, @unique1.size, sum_times(@unique1))
+      f.puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", @base2, @list2.size, sum_times(@list2), @common_count, @common_time_2, @unique2.size, sum_times(@unique2))
+    end
     puts sprintf("%#{w}s     total        common       unique", ' ')
-    puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", base1, @list1.size, sum_times(@list1), @common_count, @common_time_1, @unique1.size, sum_times(@unique1))
-    puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", base2, @list2.size, sum_times(@list2), @common_count, @common_time_2, @unique2.size, sum_times(@unique2))
+    puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", @base1, @list1.size, sum_times(@list1), @common_count, @common_time_1, @unique1.size, sum_times(@unique1))
+    puts sprintf("%#{w}s %5d %6.3f %5d %6.3f %5d %6.3f", @base2, @list2.size, sum_times(@list2), @common_count, @common_time_2, @unique2.size, sum_times(@unique2))
   end
 end
 
